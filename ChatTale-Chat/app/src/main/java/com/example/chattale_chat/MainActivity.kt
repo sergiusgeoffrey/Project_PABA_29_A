eb33e7fc7f0504aaa9dd29d26dc0539ec2c48df6
illegallyCrushed <nicholassebastianveron@gmail.com>
2021-12-23 12:50:28 +0700

    dbs

--- ChatTale-Chat/app/src/main/java/com/example/chattale_chat/MainActivity.kt
+++ ChatTale-Chat/app/src/main/java/com/example/chattale_chat/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.chattale_chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +11,12 @@ class MainActivity : AppCompatActivity() {
         lateinit var CurrentAccount : Account
         lateinit var ChatroomList : MutableList<Chatroom>
         lateinit var CurrentChatRoom: Chatroom
+        lateinit var DB : FirebaseFirestore
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        DB = FirebaseFirestore.getInstance()
     }
 }
