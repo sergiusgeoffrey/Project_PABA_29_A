c501176854c1fb45849f09c80e604d34d98f5fcb
illegallyCrushed <nicholassebastianveron@gmail.com>
2021-12-27 19:48:00 +0700

    fix lowercase

--- ChatTale-Chat/app/src/main/java/com/example/chattale_chat/NewChatFragment.kt
+++ ChatTale-Chat/app/src/main/java/com/example/chattale_chat/NewChatFragment.kt
@@ -11,7 +11,6 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.tasks.await
 import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -20,9 +19,8 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 /**
- * A simple [Fragment] subclass.
- * Use the [NewChatFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * A simple [Fragment] subclass. Use the [NewChatFragment.newInstance] factory method to create an
+ * instance of this fragment.
  */
 class NewChatFragment : Fragment() {
     // TODO: Rename and change types of parameters
@@ -38,8 +36,9 @@ class NewChatFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_new_chat, container, false)
@@ -63,15 +62,12 @@ class NewChatFragment : Fragment() {
         newChatRecyclerView.adapter = NewMemberAdapter(memberSet)
 
         // back button
-        newChatBackButton.setOnClickListener {
-            findNavController().popBackStack()
-        }
+        newChatBackButton.setOnClickListener { findNavController().popBackStack() }
 
         // add member from text field
         addMemberButton.setOnClickListener {
             // WARN! no checks!
             var newMemberString = newMemberInput.text.toString()
-            newMemberString = newMemberString.lowercase()
             newMemberInput.setText("")
 
             // adds to set and refresh recycler view
@@ -98,30 +94,42 @@ class NewChatFragment : Fragment() {
 
             // create null last message
             val chatroomLastMessage =
-                Message(null, randomUUIDString, MainActivity.CurrentAccount.username, unixTime, null, false)
+                    Message(
+                            null,
+                            randomUUIDString,
+                            MainActivity.CurrentAccount.username,
+                            unixTime,
+                            null,
+                            false
+                    )
 
             // creates new chatroom
-            val newChatroom = Chatroom(
-                randomUUIDString,
-                chatroomDisplayName,
-                chatroomMembers,
-                chatroomLastMessage
-            )
+            val newChatroom =
+                    Chatroom(
+                            randomUUIDString,
+                            chatroomDisplayName,
+                            chatroomMembers,
+                            chatroomLastMessage
+                    )
 
             MainActivity.CurrentChatroom = newChatroom
 
             // set to firebase
-            MainActivity.DB.collection("Chatrooms").document(randomUUIDString).set(newChatroom).addOnSuccessListener {
-                findNavController().navigate(R.id.action_newChatFragment_to_chattingFragment)
-            }
+            MainActivity.DB
+                    .collection("Chatrooms")
+                    .document(randomUUIDString)
+                    .set(newChatroom)
+                    .addOnSuccessListener {
+                        findNavController()
+                                .navigate(R.id.action_newChatFragment_to_chattingFragment)
+                    }
         }
-
     }
 
     companion object {
         /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
+         * Use this factory method to create a new instance of this fragment using the provided
+         * parameters.
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
@@ -130,11 +138,12 @@ class NewChatFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            NewChatFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                NewChatFragment().apply {
+                    arguments =
+                            Bundle().apply {
+                                putString(ARG_PARAM1, param1)
+                                putString(ARG_PARAM2, param2)
+                            }
                 }
-            }
     }
 }
